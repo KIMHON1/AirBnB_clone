3394558aea189de922f5fa3cc4dc8ebe12dc099c
KIMHON1 <honore.kimenyi@gmail.com>
2022-08-08 17:41:15 +0200

    create an instance with this dictionary representation.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is the first step towards building my first full web application: the AirBnB clone. This first step is very important because i will use what you build during this project with all other following projects: HTML/CSS templating, database storage, API, front-end integration…
+       ____base_model
+         To get the value associated with a specific key 
+            that does not exist in the Python dictionary, use 
+            the **kwargs.get()
+            The getattr() method returns the value of the named attribute of an object.
+            If not found, it returns the default value provided to the function.
+            Python provides the strptime() method, in its datetime class, to convert
+            a string representation of the​ date/time into a date object.
+        
--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,48 @@
+from multiprocessing.sharedctypes import Value
 import uuid
 from datetime import datetime
-
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     
     """_summary_
      class BaseModel that defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """
+            Initialization of the base model"
+            
+            
+        """
         self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                 if key != "__class__":
+                    setattr(self, key, value)
+                 if kwargs.get("created_at", None) and type(self.created_at) is str:
+                    self.created_at = datetime.strptime(kwargs["created_at"], time)
+                    
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+                    
+                    
+                    
+                
+                
+                
+                    
+             
+        
   
+
+
+
+
     
 
     def __str__(self):
